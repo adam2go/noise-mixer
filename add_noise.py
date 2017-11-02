f482b6a45b282889fbbee738ce60429071ef12bf
Adam2go <leeapply@163.com>
2017-11-02 16:24:52 +0800

    update produce.sh

--- add_noise.py
+++ add_noise.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
 '''
-    This model mix given noise wav and original wav file.
+    This module mix given noise wav and original wav file.
 '''
 
 from __future__ import print_function
@@ -37,7 +38,7 @@ def energy(mat):
 def mix(mat, noise, pos, scale):
     ret = []
     l = len(noise)
-    for i in xrange(len(mat)):
+    for i in range(len(mat)):
         frm = mat[i]
         tmp = int(frm + scale * noise[pos])
         tmp = max(min(tmp, 32767), -32768)
@@ -57,7 +58,7 @@ def mix(mat, noise, pos, scale):
 def dirichlet(params):
     samples = [random.gammavariate(x, 1) if x > 0 else 0. for x in params]
     samples = [x / sum(samples) for x in samples]
-    for x in xrange(1, len(samples)):
+    for x in range(1, len(samples)):
         samples[x] += samples[x - 1]
     return bisect.bisect_left(samples, random.random())
 
@@ -78,14 +79,29 @@ def scp(fname):
         for line in file:
             yield tuple(line.strip().split())
 
+'''
+    Description: generate int type matrix for given wav file
+    Inputs:
+        fname: wav file name
+    Outputs:
+        matrix of the wav file
+'''
 def wave_mat(fname):
     with wave.open(fname, 'r') as wav:
         num = wav.getnframes()
         data = wav.readframes(num)
         return list(struct.unpack('{}h'.format(num), data))
 
-def wave_header(sample_array, sample_rate):
-    byte_count = (len(sample_array)) * 2                            # short
+'''
+    Description: generate wav header
+    Inputs:
+        mat: wav matrix
+        rate: wav frame rate
+    Outputs:
+        packed wav header
+'''
+def wave_header(mat, rate):
+    byte_count = (len(mat)) * 2                                     # short type
     header = struct.pack('<ccccIccccccccIHHIIHH',
                       'R', 'I', 'F', 'F',
                       byte_count + 0x2c - 8,                        # header size
@@ -93,19 +109,36 @@ def wave_header(sample_array, sample_rate):
                       0x10,                                         # size of 'fmt ' header
                       1,                                            # format 1
                       1,                                            # channels
-                      sample_rate,                                  # samples / second
-                      sample_rate * 2,                              # bytes / second
+                      rate,                                         # samples / second
+                      rate * 2,                                     # bytes / second
                       2,                                            # block alignment
                       16)                                           # bits / sample
     header += struct.pack('<ccccI',
                        'd', 'a', 't', 'a', byte_count)
     return header
 
+'''
+    Description: output wav file (string type) if file directory didn't given
+    Inputs:
+        tag: wav file name/tag
+        mat: wav matrix
+    Outputs:
+        string type wav file
+'''
 def output(tag, mat):
     sys.stdout.write(tag + ' ')
     sys.stdout.write(wave_header(mat, 16000))
     sys.stdout.write(struct.pack('{}h'.format(len(mat)), *mat))
 
+'''
+    Description: output wav file (wav type) to given directory
+    Inputs:
+        dir: output directory
+        tag: wav file name/tage
+        mat: wav matrix
+    Outputs:
+        wav type file
+'''
 def output_wave_file(dir, tag, mat):
     if not os.path.exists(dir):
         os.makedirs(dir)
@@ -113,18 +146,20 @@ def output_wave_file(dir, tag, mat):
         file.write(wave_header(mat, 16000))
         file.write(struct.pack('{}h'.format(len(mat)), *mat))
 
+
 def main():
     parser = optparse.OptionParser()
-    parser.add_option('--noise_level', type=float, help='')
-    parser.add_option('--noise_src', type=str, help='')
-    parser.add_option('--noise_prior', type=str, help='')
-    parser.add_option('--seed', type=int, help='')
-    parser.add_option('--sigma0', type=float, help='')
-    parser.add_option('--verbose', type=int, help='')
-    parser.add_option('--wav_src', type=str, help='')
-    parser.add_option('--wav_dir', type=str, help='')
+    parser.add_option('--seed', type=int, default=64)
+    parser.add_option('--sigma0', type=float, default=0)
+    parser.add_option('--verbose', type=int, default=0)
+    parser.add_option('--wav_src', type=str)
+    parser.add_option('--wav_dir', type=str)
+    parser.add_option('--noise_src', type=str)
+    parser.add_option('--noise_prior', type=str)
+    parser.add_option('--noise_level', type=float)
     (args, dummy) = parser.parse_args()
     random.seed(args.seed)
+    logging.debug('{}'.format(args.noise_prior))
     params = [float(x) for x in args.noise_prior.split(',')]
 
     if args.verbose:
@@ -133,45 +168,34 @@ def main():
     noise_energies = [0.]
     noises = [(0, [])]
     for tag, wav in scp(args.noise_src):
-        # logging.debug('noise wav: %s', wav)
         mat = wave_mat(wav)
         eng = energy(mat)
-        # logging.debug('noise energy: %f', e)
         noise_energies.append(eng)
         noises.append((0, mat))
 
     for tag, wav in scp(args.wav_src):
-        # logging.debug('wav: %s', wav)
         noise_level = random.gauss(args.noise_level, args.sigma0)
-        # logging.debug('noise level: %f', noise_level)
         mat = wave_mat(wav)
         signal = energy(mat)
-        # logging.debug('signal energy: %f', signal)
         noise = signal / (10 ** (noise_level / 10.))
-        # logging.debug('noise energy: %f', noise)
         type = dirichlet(params)
-        # logging.debug('selected type: %d', type)
         fname = os.path.basename(wav)
         if type == 0:
-            if args.wavdir != 'NULL':
-                output_wave_file(args.wavdir, fname, mat)
+            if args.wav_dir != 'NULL':
+                output_wave_file(args.wav_dir, fname, mat)
             else:
                 output(tag, mat)
         else:
             p, n = noises[type]
-            #print("p: {}".format(p))
             if p + len(mat) > len(n):
                 noise_energies[type] = energy(n[p::] + n[0:len(n) - p:])
             else:
                 noise_energies[type] = energy(n[p:p + len(mat):])
-            #print("noise: {}  noise_energy: {}  type: {}".format(noise, noise_energies[type], type))
             scale = math.sqrt(noise / noise_energies[type])
-            #print("scale: {}".format(scale))
-            #logging.debug('noise scale: %f', scale)
             pos, result = mix(mat, n, p, scale)
             noises[type] = (pos, n)
-            if args.wavdir != 'NULL':
-                output_wave_file(args.wavdir, fname, result)
+            if args.wav_dir != 'NULL':
+                output_wave_file(args.wav_dir, fname, result)
             else:
                 output(tag, result)
 
---
+++ produce.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# use this script to generate noise-mixed data
+# the script add_noise.py is used to achieve the noise injection
+
+# define noise type first
+noise_prior_white="0,10,0,0"     #define white noise to sample. [S_clean, S_white, S_car, S_cafe]
+noise_prior_car="0,0,10,0"       #define car noise to sample. [S_clean, S_white, S_car, S_cafe]
+noise_prior_cafe="0,0,0,10"      #define cafe noise to sample. [S_clean, S_white, S_car, S_cafe]
+declare -A noise_prior_box=(["white"]=${noise_prior_white} ["car"]=${noise_prior_car} ["cafe"]=${noise_prior_cafe})
+
+seed=32
+sigma0=0                            #ensure the SNR is sampled as the value exacted defined by noise_level
+verbose=0
+noise_level=20
+wav_scp="`pwd`/wav_home.scp"
+noise_scp="`pwd`/noise_home.scp"
+
+for noise_type in white car cafe; do
+  noise_prior="${noise_prior_box["$noise_type"]}"
+  output_dir="`pwd`/${noise_type}/${noise_level}"
+
+  echo "producing ${noise_type} data."
+  echo "sigma0=${sigma0} noise_level=${noise_level} prior=${noise_prior}"
+  mkdir -p $output_dir
+  python add_noise.py --seed $seed --sigma0 $sigma0 --noise_level $noise_level   \
+	  --verbose $verbose --noise_prior=$noise_prior --noise_src $noise_scp  \
+	  --wav_src $wav_scp --wav_dir $output_dir || exit
+done
+
+echo "All done!"
